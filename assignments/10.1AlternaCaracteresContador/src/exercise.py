97f100682b8cb4cf86fe3e54b83243c0fa4cfea4
GABRIEL HERNÁNDEZ GARZA <A01723516@tec.mx>
2024-09-17 20:53:57 -0600

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+    numero=int(input("ingresa un numero"))
+    for i in range (1,numero+1):
+        if i%2==0:
+            print(str(i)+"-%")
+        else:
+            print(str(i)+"-#")
 
 if __name__=='__main__':   
     main()
