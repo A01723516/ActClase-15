6f8d536d64ef15fe1644bbf559536dbb9ddeaf0f
GABRIEL HERNÁNDEZ GARZA <A01723516@tec.mx>
2024-09-17 20:48:50 -0600

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,6 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+cuenta=0
+numero=0
+suma=0
+while numero>=0:
+    numero=float(input())
+    if numero>0:
+        suma+=numero
+        cuenta+=1
+    if numero<0:
+        break
+    
+promedio=(suma/cuenta)
+print(promedio)
 
 if __name__=='__main__':
     main()
