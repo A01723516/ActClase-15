8b5a76cbe162b323e32444844ba8041ebc74d0dd
GABRIEL HERNÁNDEZ GARZA <A01723516@tec.mx>
2024-09-17 20:53:05 -0600

    Update exercise.py

--- assignments/08RangoParesSimple/src/exercise.py
+++ assignments/08RangoParesSimple/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+    a=int(input())
+    b=int(input())
+    for numero in range (a,b+1):
+        if numero%2==0:
+            print (numero)
 
 if __name__=='__main__':
     main()
