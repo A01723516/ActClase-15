47bba9c26dd0c3ad1c2f9b362956bce203defa49
GABRIEL HERNÁNDEZ GARZA <A01723516@tec.mx>
2024-09-17 20:51:44 -0600

    Update exercise.py

--- assignments/07CantidadPares/src/exercise.py
+++ assignments/07CantidadPares/src/exercise.py
@@ -1,6 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+    cuenta=0
+    numeros=0
+    while numeros >=0:
+        numero=float(input())
+        if numero%2==0:
+            cuenta+=1
+        if numero ==0:
+            cuenta+=1
+        if numero < 0:
+            break
+    print("Total de pares="+str(cuenta))
+     
 
 if __name__=='__main__':
     main()
